95fa81ff0c129e98b54ce7357caf0ab5c1f24892
z <z.y.j.1379@gmail.com>
2022-03-16 20:02:21 +0330

    first implementation of sqrt_div

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,7 +1,13 @@
 package calculator;
 
+import java.lang.Math;
+
 public class Calculator {
     public int add(int a, int b) {
         return a + b;
+    }
+
+    public double sqrt_div(int a, int b) {
+        return Math.sqrt(a / b);
     }
 }
